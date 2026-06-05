8c96448722e513b9a1f72b6723fde7a926aa02dd
Max <maximilian.kenn@gmail.com>
2026-06-05 11:18:27 +0200

    implement service list component with routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ServiceList } from './service-list/service-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ServiceList,
+    }
+];
