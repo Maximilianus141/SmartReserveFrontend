d56b9f487b1f9335208ce45dcee9b245b85e50c3
Maximilian Kenner <maximilian.kenn@gmail.com>
2026-06-06 12:06:52 +0200

    feat: enhance booking-select calendar with date navigation and dynamic date rendering

--- src/app/components/booking-select-calender/booking-select-calender.ts
+++ src/app/components/booking-select-calender/booking-select-calender.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CalenderDate } from "../calender-date/calender-date";
 
 @Component({
 	selector: 'app-booking-select-calender',
-	imports: [],
+	imports: [CalenderDate],
 	templateUrl: './booking-select-calender.html',
 	styleUrl: './booking-select-calender.css',
 })
@@ -10,4 +11,54 @@ export class BookingSelectCalender {
 	selectedDate: Date | null = null;
 	curerntlySelectedMonth: number = new Date().getMonth();
 	currentlySelectedYear: number = new Date().getFullYear();
+	months: string[] = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+
+
+	dates: number[] = new Array(35).fill(0);
+
+	resetDates() {
+		this.dates.fill(0);
+	}
+
+	nextMonth() {
+		if (this.curerntlySelectedMonth === 11) {
+			this.curerntlySelectedMonth = 0;
+			this.currentlySelectedYear++;
+		} else {
+			this.curerntlySelectedMonth++;
+		}
+		this.setDates();
+	}
+
+	prevMonth() {
+		if (this.curerntlySelectedMonth === 0) {
+			this.curerntlySelectedMonth = 11;
+			this.currentlySelectedYear--;
+		} else {
+			this.curerntlySelectedMonth--;
+		}
+		this.setDates();
+	}
+
+	ngOnInit() {
+		this.setDates();
+	}
+
+	setDates() {
+		this.resetDates();
+		const now = new Date(this.currentlySelectedYear, this.curerntlySelectedMonth, 1);
+		let today = now.getDay() - 1;
+		if (today < 0) {
+			today = 6;
+		}		
+
+		const daysInMonth = new Date(this.currentlySelectedYear, this.curerntlySelectedMonth + 1, 0).getDate();
+
+		for (let i = 0; i < daysInMonth; i++) {
+			this.dates[today + i] = i + 1;
+		}
+	}
+
+
+
 }
