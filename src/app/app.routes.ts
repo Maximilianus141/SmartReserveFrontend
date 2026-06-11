4043c08af6c7f928b68f74f84201c6439c61af55
Max <maximilian.kenn@gmail.com>
2026-06-11 13:38:40 +0200

    feat: add reservation management component with editing functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ServiceList } from './pages/service-list/service-list';
 import { BookingSelect } from './pages/booking-select/booking-select';
 import { ServiceEdit } from './pages/service-edit/service-edit';
 import { authGuard } from './auth/auth.guard';
+import { ReservationManagement } from './pages/reservation-management/reservation-management';
 
 export const routes: Routes = [
 	{
@@ -19,4 +20,10 @@ export const routes: Routes = [
 		canActivate: [authGuard],
 		data: { roles: ['ROLE_admin'] },
 	},
+	{
+		path: 'reservations',
+		component: ReservationManagement,
+		canActivate: [authGuard],
+		data: { roles: ['ROLE_guest'] },
+	}
 ];
